e8a1db0127cd3bdeba781591816a1c7035bdb2ab
liling3 <2649808385@qq.com>
2021-12-20 15:19:26 +0800

    add firstPte

---
+++ src/views/helloWorld/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>hello {{name}}!</h1>
+  </div>
+</template>
+ 
+<script>
+export default {
+  data() {
+    return {
+      name: "world"
+    };
+  }
+};
+</script>
+ 
+<style lang="less" scoped>
+h1 {
+  color: red;
+}
+</style>
